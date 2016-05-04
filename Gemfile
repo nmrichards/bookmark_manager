0c4e8362d6ed3df61a5fd246c6afb8c6f2d7b9c3
Nick <nm.richards@yahoo.com>
2016-05-04 07:12:21 -0400

    Challenge 10

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.3.0'
 gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'rspec'
 
 group :development, :testing do
   gem 'capybara'
---
+++ app/app.rb
@@ -0,0 +1,13 @@
+require 'sinatra/base'
+require_relative 'models/link.rb'
+
+class BookmarkManager < Sinatra::Base
+  get '/links' do
+    erb(:links)
+  end
+
+
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
