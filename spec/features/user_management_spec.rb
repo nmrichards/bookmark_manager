61fa1aef619384393c272f979189624e0a94e084
Nick <nm.richards@yahoo.com>
2016-05-05 15:19:55 -0400

    Challenge 19

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -4,4 +4,13 @@ feature 'User sign up' do
     expect(page).to have_content('Welcome, johnsmith@gmail.com')
     expect(User.first.email).to eq('johnsmith@gmail.com')
   end
+
+  scenario "A user won't be created if passwords are mismatched" do
+    visit '/users/new'
+    fill_in :email, with: 'johnsmith@gmail.com'
+    fill_in :password, with: 'oranges!'
+    fill_in :password_confirmation, with: 'apples?'
+    click_button 'Sign up'
+    expect(User.count).to eq 0
+  end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -10,5 +10,6 @@ def sign_up
   visit '/users/new'
   fill_in :email, with: 'johnsmith@gmail.com'
   fill_in :password, with: 'oranges!'
+  fill_in :password_confirmation, with: 'oranges!'
   click_button 'Sign up'
 end
